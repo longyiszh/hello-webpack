3c7aed4cfe0ec0972f9169fb073e6945d17a6c33
Valorad <wcxaaa@gmail.com>
2017-08-31 14:57:25 +0800

    * bootstrap and jquery are now handled by webpack

---
+++ src/testMain.js
@@ -0,0 +1,23 @@
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "./css/lrtHP.css";
+import "./css/mapFO4.css";
+import "./css/mapWitcher3.css";
+
+// scss
+import "./scss/main.scss";
+
+// js
+import * as bootstrap from 'bootstrap';
+
+import { gaoxiao, hahaha } from "./js/testGaoxiao";
+
+const a4 = 2;
+
+const count = function () {
+    console.log(gaoxiao + a4);
+}
+
+count();
+hahaha();
